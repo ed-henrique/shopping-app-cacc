c592d65eaef98002ca4861b3781af3a34b10487d
Guilherme Bernardo <bguilherme51@gmail.com>
2023-01-18 19:23:04 -0400

    Update product_model.dart

--- lib/models/product_model.dart
+++ lib/models/product_model.dart
@@ -13,77 +13,77 @@ class Product {
     Product(
       name: "amendoim",
       price: 1.0,
-      imageUrl: "assets/images/amendoim.png",
+      imageUrl: "../../assets/images/amendoim.png",
     ),
     Product(
       name: "baré",
       price: 3.5,
-      imageUrl: "assets/bare.jpg",
+      imageUrl: "../../assets/bare.jpg",
     ),
     Product(
       name: "bis",
       price: 0.5,
-      imageUrl: "assets/bis.jfif",
+      imageUrl: "../../assets/bis.jfif",
     ),
     Product(
       name: "biscoito mabel",
       price: 3.0,
-      imageUrl: "assets/mabel.webp",
+      imageUrl: "../../assets/mabel.webp",
     ),
     Product(
       name: "biscoito spantoo",
       price: 3.0,
-      imageUrl: "assets/spantoo.jfif",
+      imageUrl: "../../assets/spantoo.jfif",
     ),
     Product(
       name: "chocolate baton",
       price: 1.5,
-      imageUrl: "assets/baton.webp",
+      imageUrl: "../../assets/baton.webp",
     ),
     Product(
       name: "chocolate kitkat",
       price: 4.0,
-      imageUrl: "assets/kitkat.jfif",
+      imageUrl: "../../assets/kitkat.jfif",
     ),
     Product(
       name: "coca cola",
       price: 3.5,
-      imageUrl: "assets/coca.jfif",
+      imageUrl: "../../assets/coca.jfif",
     ),
     Product(
       name: "cup noodles",
       price: 6.5,
-      imageUrl: "assets/cupnoodles.jpeg",
+      imageUrl: "../../assets/cupnoodles.jpeg",
     ),
     Product(
       name: "energético monster",
       price: 8.5,
-      imageUrl: "",
+      imageUrl: "../../assets/images/monster.jpg",
     ),
     Product(
       name: "fanta",
       price: 3.5,
-      imageUrl: "",
+      imageUrl: "../../assets/images/fanta.jfif",
     ),
     Product(
       name: "fregells",
       price: 1.5,
-      imageUrl: "",
+      imageUrl: "../../assets/images/freegells.jfif",
     ),
     Product(
       name: "hersheys",
       price: 0.4,
-      imageUrl: "",
+      imageUrl: "../../assets/images/hersheys.jfif",
     ),
     Product(
       name: "paçoquinha",
       price: 0.75,
-      imageUrl: "",
+      imageUrl: "../../assets/images/paçoquinha.jfif",
     ),
     Product(
       name: "schweppes citrus",
       price: 4.0,
-      imageUrl: "",
+      imageUrl: "../../assets/images/schweppes.jpg",
     ),
     Product(
       name: "snacks yes",
@@ -93,12 +93,12 @@ class Product {
     Product(
       name: "toddynho",
       price: 2.5,
-      imageUrl: "",
+      imageUrl: "../../assets/images/toddynho.jpg",
     ),
     Product(
       name: "tuffit",
       price: 2.0,
-      imageUrl: "",
+      imageUrl: "../../assets/images/tuffit.png",
     )
   ];
 }
